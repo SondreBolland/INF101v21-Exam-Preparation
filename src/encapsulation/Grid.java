84d9253dc5cc4aa73b0ae3440ea77d9fd83dbf9f
Sondre.Bolland1 <sondre.bolland@student.uib.no>
2021-05-24 13:54:53 +0200

    Finished presentation

--- src/encapsulation/Grid.java
+++ src/encapsulation/Grid.java
@@ -14,11 +14,20 @@ public class Grid {
 		grid = new double[rows][columns];
 	}
 	
+	/**
+	 * Set element in grid at coordinates x and y.
+	 * No values above 100.0 are allowed.
+	 * @param x coordinate
+	 * @param y coordinate
+	 * @param value, element to set in the grid
+	 */
 	public void set(int x, int y, double value) {
 		if (x < 0 || x >= rows)
 			throw new IndexOutOfBoundsException("The x coordinate is out of bounds");
 		if (y < 0 || y >= columns)
 			throw new IndexOutOfBoundsException("The y coordinate is out of bounds");
+		if (value > 100.0)
+			throw new IllegalArgumentException("No values above 100.0 are allowed");
 		grid[x][y] = value;
 	}
 	
